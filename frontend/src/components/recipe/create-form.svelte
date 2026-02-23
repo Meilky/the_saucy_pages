157bada19d719fb704e0043acabedf35be19dcf6
Meilky <mikaeljean2k3@gmail.com>
2026-02-23 12:06:36 -0500

    feat: added ingredients to recipe creation

--- frontend/src/components/recipe/create-form.svelte
+++ frontend/src/components/recipe/create-form.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createRecipe } from "$api/recipes";
+	import type { RecipeIngredient } from "../../models/recipe";
 
 	interface Props {
 		onSubmited?: () => void;
@@ -9,29 +10,112 @@
 
 	let name = $state("");
 	let description = $state("");
+	let ingredients: RecipeIngredient[] = $state([]);
+
+	function reset() {
+		name = "";
+		description = "";
+		ingredients = [];
+	}
+
+	async function submit() {
+		await createRecipe({
+			name,
+			description,
+			ingredients,
+		});
+
+		reset();
+
+		props.onSubmited?.();
+	}
 
 	async function onFormSubmit(e: SubmitEvent) {
 		e.preventDefault();
 
-		await createRecipe({
-			name,
-			description,
+		await submit();
+	}
+
+	function onFormReset(e: any) {
+		e.preventDefault();
+
+		reset();
+	}
+
+	function addIngredient() {
+		ingredients.push({
+			uuid: "123",
+			amount: 14,
 		});
+	}
 
-		name = "";
-		description = "";
+	function removeIngredient(uuid: string) {
+		const idx = ingredients.findIndex((v) => v.uuid === uuid);
 
-		props.onSubmited?.();
+		if (idx === -1) return;
+
+		ingredients.splice(idx, 1);
+	}
+
+	function createRemoveRecipeCallback(uuid: string) {
+		return () => {
+			return removeIngredient(uuid);
+		};
+	}
+
+	function createEditRecipeCallback(uuid: string) {
+		return () => {
+			console.log(`Edit: ${uuid}`);
+		};
 	}
 </script>
 
-<form onsubmit={onFormSubmit}>
+<form onsubmit={onFormSubmit} onreset={onFormReset}>
 	<fieldset>
 		<legend>New recipe</legend>
 		<label for="name">Name</label>
 		<input type="text" name="name" bind:value={name} />
 		<label for="description">Description</label>
 		<input type="text" name="description" bind:value={description} />
+		<table>
+			<thead>
+				<tr>
+					<th>Ingredient</th>
+					<th>Amount</th>
+					<th>
+						<button type="button" onclick={addIngredient}>
+							Add
+						</button>
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each ingredients as ingredient}
+					<tr>
+						<td>{ingredient.uuid}</td>
+						<td>{ingredient.amount}</td>
+						<td>
+							<button
+								type="button"
+								onclick={createRemoveRecipeCallback(
+									ingredient.uuid,
+								)}
+							>
+								Remove
+							</button>
+							<button
+								type="button"
+								onclick={createEditRecipeCallback(
+									ingredient.uuid,
+								)}
+							>
+								Edit
+							</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</fieldset>
 	<input type="submit" value="Submit" />
 	<input type="reset" value="Reset" />
@@ -41,5 +125,10 @@
 	fieldset {
 		display: grid;
 		grid-template-columns: auto auto;
+	}
+
+	table {
+		grid-column-start: 1;
+		grid-column-end: 3;
 	}
 </style>
--- frontend/src/models/recipe.ts
+++ frontend/src/models/recipe.ts
@@ -1,10 +1,17 @@
+export type RecipeIngredient = {
+	uuid: string;
+	amount: number;
+}
+
 export type Recipe = {
 	uuid: string;
 	name: string;
 	description: string;
+	ingredients: RecipeIngredient[];
 };
 
 export type CreateRecipe = {
 	name: string;
 	description: string;
+	ingredients: RecipeIngredient[];
 };
